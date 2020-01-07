10145b4f0ee928fdc034d3f96733b4e25e0d08ba
damith <dwanninayaka@gmail.com>
2020-01-07 23:56:56 +0800

    Initial commit

---
+++ src/main/java/com/idotrick/i18n/springrest/SpringBootRestI18nApplication.java
@@ -0,0 +1,13 @@
+package com.idotrick.i18n.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestI18nApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestI18nApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idotrick/i18n/springrest/SpringBootRestI18nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idotrick.i18n.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestI18nApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
